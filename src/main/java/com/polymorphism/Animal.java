8265782229f088c0a3f8e18d9333ed4921e67e04
Nileshlad <60500269+Nileshlad@users.noreply.github.com>
2020-05-15 13:30:34 +0530

    Added: To simple example polymorphism

---
+++ src/main/java/com/polymorphism/Animal.java
@@ -0,0 +1,34 @@
+package com.polymorphism;
+
+public class Animal {
+    public void sound(){
+        System.out.println("Animal is making a sound");
+    }
+}
+
+class Horse extends Animal{
+    @Override
+    public void sound(){
+        System.out.println("Neigh");
+    }
+    public static void main(String args[]){
+        Animal obj = new Horse();
+        obj.sound();
+    }
+}
+
+//out put
+//Neigh
+class Cat extends Animal{
+    @Override
+    public void sound(){
+        System.out.println("Meow");
+    }
+    public static void main(String args[]){
+        Animal obj = new Cat();
+        obj.sound();
+    }
+}
+
+//output
+//Meow
